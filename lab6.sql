b4996994b458464c513ab8149e155b8b8e59c7cf
Marek Lewiński <lewinski.marek@outlook.com>
2018-05-21 10:55:36 +0200

    Implemented dynamic SQL query

--- lab6.sql
+++ lab6.sql
@@ -70,7 +70,7 @@ create or replace procedure
 	pN_USER_update (name in varchar2, new_name in varchar2)
 	is
 	begin
-		for dane in (select USE_NAME from USERS where USE_NAME like 'Uzytkownik')
+		for dane in (select USE_NAME from USERS where USE_NAME like name)
 			loop
 				exit when
 					SQL%NOTFOUND OR
@@ -102,3 +102,43 @@ create or replace procedure
 	Uzytkownik
 */
 exec pN_USER_update('Imie', 'No name');
+/*
+    USE_ID USE_NAME
+---------- ------------------------------
+         1 No name
+         4 No name
+         5 No name
+         6 No name
+         7 No name
+         8 No name
+         9 No name
+        10 No name
+        11 No name
+        12 No name
+        13 No name
+
+    USE_ID USE_NAME
+---------- ------------------------------
+         2 Piotr
+         3 Tomasz
+        14 Uzytkownik
+        15 Uzytkownik
+*/
+
+---------------------------------------------------------------------
+CREATE or REPLACE PROCEDURE pINSERT_GROUP(G_NAME IN VARCHAR2, G_DESC IN VARCHAR2)
+IS
+	insert_query VARCHAR(500); 
+BEGIN
+	insert_query := 'INSERT INTO GROUPS(GRO_NAME, GRO_DESCRIPTION) VALUES (:1, :2)';
+	EXECUTE IMMEDIATE insert_query USING G_NAME, G_DESC;
+    
+END;
+/
+
+exec pINSERT_GROUP('Nowa grupa dynamiczna','Grupa utworzona dynamicznym zapytaniem SQL');
+
+/*
+	22 user11 Private group of user
+	41 Nowa grupa dynamiczna Grupa utworzona dynamicznym zapytaniem SQL
+*/
